035da2149ae194287764a811ddff5d1f6d0a590e
Raghukongala <kongalaraghu406@gmail.com>
2026-03-23 01:07:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Banking Scheduler Running"
+    return "Banking Scheduler  Raghu Running"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
